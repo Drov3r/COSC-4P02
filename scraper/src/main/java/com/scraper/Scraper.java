475a50a46261559314bed4edf86f8299adfd6251
griffin <gallen1999@live.com>
2022-03-04 22:22:35 -0500

    basic sport schedule scraping

--- scraper/src/main/java/com/scraper/Scraper.java
+++ scraper/src/main/java/com/scraper/Scraper.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.regex.Matcher;
@@ -17,11 +18,20 @@ import com.gargoylesoftware.htmlunit.WebRequest;
 import com.gargoylesoftware.htmlunit.html.DomNode;
 import com.gargoylesoftware.htmlunit.html.DomNodeList;
 import com.gargoylesoftware.htmlunit.html.HtmlButton;
+import com.gargoylesoftware.htmlunit.html.HtmlData;
+import com.gargoylesoftware.htmlunit.html.HtmlDataList;
 import com.gargoylesoftware.htmlunit.html.HtmlOption;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlSelect;
 import com.gargoylesoftware.htmlunit.html.HtmlTable;
+import com.gargoylesoftware.htmlunit.html.HtmlTableBody;
+import com.gargoylesoftware.htmlunit.html.HtmlTableCell;
+import com.gargoylesoftware.htmlunit.html.HtmlTableDataCell;
+import com.gargoylesoftware.htmlunit.html.HtmlTableFooter;
+import com.gargoylesoftware.htmlunit.html.HtmlTableHeader;
+import com.gargoylesoftware.htmlunit.html.HtmlTableRow;
 import com.gargoylesoftware.htmlunit.javascript.host.Element;
+import com.gargoylesoftware.htmlunit.javascript.host.html.HTMLTableDataCellElement;
 
 import org.apache.commons.logging.LogFactory;
 import org.jsoup.Jsoup;
@@ -75,59 +85,9 @@ public abstract class Scraper {
             doc = Jsoup.connect("https://niagara2022games.ca/about/alumni/").get();
         }
         else if (type.equals("sports")){
-            webClient.getOptions().setJavaScriptEnabled(true);
-            webClient.getOptions().setThrowExceptionOnFailingStatusCode(true);
-            webClient.getOptions().setThrowExceptionOnScriptError(true);
-            webClient.setAjaxController(new NicelyResynchronizingAjaxController());
-            WebRequest req = new WebRequest(new URL("https://cg2022.gems.pro/Result/Calendar.aspx"));
-            HtmlPage myPage = webClient.getPage(req);
-            //System.out.println(myPage.asNormalizedText());
-            int i = webClient.waitForBackgroundJavaScript(1000);
-            //System.out.println(i);
-            HtmlSelect sel = (HtmlSelect) myPage.getElementById("ctl00_ctl00_ContentPlaceHolderBasicMaster_ContentPlaceHolder1_selGameDay");
-            sel.click();
-            for (DomNode el : sel.getDomElementDescendants()) {
-                if (el.getTextContent().contains("Tuesday, August 16, 2022")){
-                    HtmlOption op = (HtmlOption) el;
-                    op.click();
-                }
-                System.out.println(el.getTextContent());
-            }
-
-            sel = (HtmlSelect) myPage.getElementById("ctl00_ctl00_ContentPlaceHolderBasicMaster_ContentPlaceHolder1_selSport");
-            sel.click();
-            for (DomNode el : sel.getDomElementDescendants()) {
-                if (el.getTextContent().contains("Athletics")){
-                    HtmlOption op = (HtmlOption) el;
-                    op.click();
-                }
-                System.out.println(el.getTextContent());
-            }
-
-            for (DomNode table : myPage.getElementsByTagName("table")) {
-                System.out.println(table.getTextContent());
-            }
-
-            while (i>0){
-                i = webClient.waitForBackgroundJavaScript(1000);
-                if (i==0){
-                    break;
-                }
-                synchronized (myPage) {
-                    try {
-                        System.out.println("waitin");
-                        myPage.wait(500);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-            webClient.getAjaxController().processSynchron(myPage, req, false);
-            doc = Jsoup.parse(myPage.asXml());
-            System.out.println("hi yio");
-            //driver.navigate().to("https://cg2022.gems.pro/Result/Calendar.aspx?SetLanguage=en-CA");
+            
+            
         }
-        //driver.close();
     }
 
     /* for testing the desired outcome */
--- scraper/src/main/java/com/scraper/SportScraper.java
+++ scraper/src/main/java/com/scraper/SportScraper.java
@@ -2,8 +2,16 @@ package com.scraper;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import com.gargoylesoftware.htmlunit.html.DomNodeList;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlTable;
+import com.gargoylesoftware.htmlunit.html.HtmlTableBody;
+import com.gargoylesoftware.htmlunit.html.HtmlTableRow;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -21,13 +29,107 @@ public final class SportScraper extends Scraper{
     public SportScraper(String type) throws IOException {
         super(type);
 
-        Elements elements = doc.body().getElementsByClass("DataCell");
+        webClient.getOptions().setJavaScriptEnabled(true);
+            HtmlPage myPage = webClient.getPage("https://cg2022.gems.pro/Result/Calendar.aspx?SetLanguage=en-CA&Grouping=S");
 
-        /*
-        for (Element el : elements){
-            System.out.println(el.toString());
-        }
-        */
+            DomNodeList tables = myPage.getElementsByTagName("table");
+            HtmlTable table = (HtmlTable) tables.get(2);
+            
+            List<HtmlTableBody> tableBodies = table.getBodies();
+
+            ArrayList<String[]> tupleList = new ArrayList<>();
+
+            String[] tuple = new String[7];
+            String sport = "";
+            String tempSport;
+            List<HtmlTableRow> tableRows;
+            HtmlTableRow currentRow;
+            for (int i=1; i<tableBodies.size(); i++){
+                tableRows = tableBodies.get(i).getRows();
+                for (int k=0; k<tableRows.size(); k++){
+                    currentRow = tableRows.get(k);
+                    tempSport = currentRow.getCell(1).asNormalizedText();
+                    System.out.println(tempSport);
+                    tuple = new String[7];
+                    if (tempSport!=""){
+                        sport = tempSport;
+                    }
+                    else if (sport!=""){
+                        tuple[0] = sport;                                           // sport
+                        tuple[1] = currentRow.getCell(3).asNormalizedText();        // date
+                        tuple[2] = currentRow.getCell(4).asNormalizedText();        // time
+                        tuple[3] = currentRow.getCell(5).asNormalizedText();        // category
+                        tuple[4] = currentRow.getCell(6).asNormalizedText();        // subcategory
+                        tuple[5] = currentRow.getCell(7).asNormalizedText();        // location
+                        tupleList.add(tuple);
+                        items.add(tuple);
+                    }
+                }
+            }
+
+            /*
+            for (String[] tuples : tupleList) {
+                for (String t : tuples) {
+                    System.out.println(t);
+                }
+                System.out.println();
+            }
+            */
+
+            // for (String[] t : tupleList) {
+            //     String stringToParse = t[4];
+            //     // The goal is to put all substrings into categories
+            //     // index 0: bracket-level
+            //     // index 1: grouping
+            //     // index 2: game-number
+            //     // index 3 & 4: participants
+            //     String[] masterArray = {null,null,null,null,null};
+
+            //     // so lets fill the parsed string into an array,
+            //     // unsorted for now, sort the array by category in a bit
+
+            //     // split string by '|' bars
+            //     String[] bar = stringToParse.split("|");
+            //     if(bar.length > 1){        // check if the string was split at all
+            //         masterArray[0] = bar[0];   // save first part of string
+            //         stringToParse = bar[1];    // parse rest of string
+            //     }
+            //     for (String c : bar) {
+            //         System.out.println(c + ", ");
+            //     }
+            //     System.out.println();
+
+            //     // split the string by '-' dash
+            //     String[] dash = stringToParse.split("-");
+            //     for(int i = 0; i < dash.length; i++){
+            //     // check if we already filled the first index
+            //         if(masterArray[0] != null){
+            //             masterArray[i] = dash[i];
+            //         }else {
+            //             // offset by 1
+            //             masterArray[i + 1] = dash[i];
+            //         }
+            //     }
+
+            //     // if the master array is not full, check for brackets and split by those
+            //     String[] brackets;
+            //     if(masterArray[3] != null){
+            //         // get the rest of the bracket split
+            //         brackets = dash[dash.length-1].split("(");
+            //         masterArray[3] = brackets[1]; // fill master array with participants
+            //     }
+
+            //     /*
+            //     for (String a : masterArray) {
+            //         System.out.print(a + ",");
+            //     }
+            //     */
+
+
+            // }
+        
+
+            printItems();
     }
 
     public static void main(String[] args) throws IOException {
