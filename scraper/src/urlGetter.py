932da3fc7fd6545ef160927efc256de07ff62012
Aidan <55853976+aidanLarock@users.noreply.github.com>
2022-03-08 20:53:46 -0500

    Add files via upload

--- scraper/src/urlGetter.py
+++ scraper/src/urlGetter.py
@@ -1,118 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 
+def get_URL():
+    print("Getting URLs")
+    urlSt = 'https://niagara2022games.ca/'
+    reqs = requests.get(urlSt)
+    soup = BeautifulSoup(reqs.text, 'html.parser')
+    urls = []
+    for link in soup.find_all('a'):
+        linky = link.get('href')
+        if 'niagara2022games' in linky:
+            urls.append(linky)
+    
+    all = []
+    for urly in urls:
+        print(urly)
+        request = requests.get(urly)
+        soup = BeautifulSoup(request.text, 'html.parser')
+        for link in soup.find_all('a'):
+            linky = link.get('href')
+            if 'niagara2022games' in linky:
+                all.append(linky)
 
+    for url in all:
+        print(url)
 
-url = 'https://niagara2022games.ca/'
-reqs = requests.get(url)
-soup = BeautifulSoup(reqs.text, 'html.parser')
-f = open('file.txt', 'w')
-
- 
-urls = []
-for link in soup.find_all('a'):
-    linky = link.get('href')
-    if 'niagara2022games' in linky:
-        urls.append(linky) 
-
-# getting length of list
-length = len(urls)
-  
-# Iterating the index
-# same as 'for i in range(len(list))'
-with open('all.txt', 'w') as f:
-    for i in range(length):
-        f.write(urls[i]+"\n")
-
-
-url = 'https://niagara2022games.ca/about/alumni/'
-reqs = requests.get(url)
-soup = BeautifulSoup(reqs.text, 'html.parser')
- 
-urls = []
-for link in soup.find_all('a'):
-    linky = link.get('href')
-    if 'niagara2022games' in linky:
-        if 'alumni' in linky:
-            if linky not in urls:
-                urls.append(linky)
-            
-# getting length of list
-length = len(urls)
-  
-# Iterating the index
-# same as 'for i in range(len(list))'
-with open('alumni.txt', 'w') as f:
-    for i in range(length):
-        f.write(urls[i]+"\n")
-
-
-url = 'https://niagara2022games.ca/about/'
-reqs = requests.get(url)
-soup = BeautifulSoup(reqs.text, 'html.parser')
- 
-urls = []
-for link in soup.find_all('a'):
-    linky = link.get('href')
-    if 'niagara2022games' in linky:
-        if 'about' in linky:
-            if linky not in urls:
-                urls.append(linky)
-            
-# getting length of list
-length = len(urls)
-  
-# Iterating the index
-# same as 'for i in range(len(list))'
-with open('about.txt', 'w') as f:
-    for i in range(length):
-        f.write(urls[i]+"\n")
-
-
-url = 'https://niagara2022games.ca/news'
-reqs = requests.get(url)
-soup = BeautifulSoup(reqs.text, 'html.parser')
- 
-urls = []
-for link in soup.find_all('a'):
-    linky = link.get('href')
-    if 'niagara2022games' in linky:
-        if 'news' in linky:
-            if linky not in urls:
-                urls.append(linky)
-            
-# getting length of list
-length = len(urls)
-  
-# Iterating the index
-# same as 'for i in range(len(list))'
-with open('news.txt', 'w') as f:
-    for i in range(length):
-        f.write(urls[i]+"\n")
-
-
-
-url = 'https://niagara2022games.ca/events'
-reqs = requests.get(url)
-soup = BeautifulSoup(reqs.text, 'html.parser')
- 
-urls = []
-for link in soup.find_all('a'):
-    linky = link.get('href')
-    if 'niagara2022games' in linky:
-        if 'events' in linky:
-            if linky not in urls:
-                urls.append(linky)
-            
-# getting length of list
-length = len(urls)
-  
-# Iterating the index
-# same as 'for i in range(len(list))'
-with open('events.txt', 'w') as f:
-    for i in range(length):
-        f.write(urls[i]+"\n")
-
-
-
-f.close()
+    
+get_URL()
