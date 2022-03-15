ad4507c58d4b079a1cd337446fc660cd9be61cb4
gunsmoke1416 <57071455+gunsmoke1416@users.noreply.github.com>
2022-03-15 18:56:56 -0400

    a

--- Java_REST_API_Test/src/Access.java
+++ Java_REST_API_Test/src/Access.java
@@ -10,7 +10,7 @@ public class Access {
         //https://codeit.blog/2020/12/06/christmas-count-down-clock-with-java/
         LocalDate today = LocalDate.now();//getting current date
 
-        LocalDate ChristmasDay = LocalDate.of(2022, Month.JUNE, 1);// setting Christmass day
+        LocalDate ChristmasDay = LocalDate.of(2022, Month.JUNE, 1);// setting Christmas day
 
         LocalDate nextXmas = ChristmasDay.withYear(today.getYear());// Next year Christmas
 
