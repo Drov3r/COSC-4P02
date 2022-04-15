3b613b9bfc6702c73abc974d3c1d2688b3a3ab6f
griffin <gallen1999@live.com>
2022-04-15 06:58:34 -0400

    Added write to text file in scraper

--- scraper/src/main/java/com/scraper/ScraperManager.java
+++ scraper/src/main/java/com/scraper/ScraperManager.java
@@ -1,6 +1,11 @@
 package com.scraper;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+
+import org.python.antlr.ast.Print;
 
 public final class ScraperManager {
 
@@ -13,29 +18,30 @@ public final class ScraperManager {
         String[] cardStrings = { "name", "date", "description", "link", "imageURL" };
 
 
-        //updateDB(
-        //    chooseScraper("events"), 
-        //    cardStrings
-        //);
+        updateDB(
+           chooseScraper("events"), 
+           cardStrings
+        );
 
-        //updateDB(
-        //    chooseScraper("news"), 
-        //    cardStrings
-        //);
+        updateDB(
+           chooseScraper("news"), 
+           cardStrings
+        );
 
-        // updateDB(
-        //     chooseScraper("alumni"), 
-        //     cardStrings
-        // );
+        updateDB(
+            chooseScraper("alumni"), 
+            cardStrings
+        );
 
-        // updateDB(
-        //     chooseScraper("venues"),
-        //     new String[]{"venue","description","sport"});
+        updateDB(
+            chooseScraper("venues"),
+            new String[]{"venue","description","sport"});
 
         updateDB(
             chooseScraper("sports"), 
             new String[]{"sport","time","category","subCategory","location"});
 
+        generateTextFile();
     }
 
     /**
@@ -72,6 +78,38 @@ public final class ScraperManager {
         dbManager.deleteFromDB(scraper, itemsInDB, scraper.type, headers);
         dbManager.insertIntoDB(scraper, itemsInDB, scraper.type, headers);
         
+    }
+
+    public void generateTextFile(){
+        
+        PrintWriter writer;
+        try {
+            writer = new PrintWriter("all-data.txt", "UTF-8");
+            writeToFile("sports",writer);
+            writeToFile("venues", writer);
+            writeToFile("alumni", writer);
+            writeToFile("events", writer);
+            writeToFile("news", writer);
+            writer.close();
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+    }
+
+    public void writeToFile(String tableName, PrintWriter writer){
+        ArrayList<String[]> itemsInDb = dbManager.getFromDB(tableName, new String[]{"*"});
+        writer.println(tableName);
+        for (String[] arr : itemsInDb){
+            for (int i=0; i<arr.length; i++){
+                writer.print(arr[i]);
+                if ((i+1)<arr.length) writer.print(", "); 
+            }
+            writer.println();
+        }
+        writer.println();
+        writer.println();
     }
 
     public static void main(String[] args) throws IOException {
